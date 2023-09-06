5a84f2d2637fd0ec2a0fe166e978cfaaa72098f6
BautiCariaga23 <115420958+BautiCariaga23@users.noreply.github.com>
2023-09-06 14:54:27 -0300

    anims

--- script.js
+++ script.js
@@ -27,55 +27,13 @@ function ReloadPage(){
     location.reload()
 }
 
-window.addEventListener("keyup", ()=>{
-    if(event.keyCode != 8 && event.keyCode != 13){
-        //Si no es ni enter ni backspace
-        if(currRow<5){
-            if(space != null){
-                space.style.border = "none"
-                space.style.animation = ""
-            } 
-            space = document.getElementById(`column_${currColumn}:row_${currRow}`);
-            space.style.border = "2px solid black"
-            space.style.animation = "spaceAnim 0.5s forwards 0.1s infinite"
-            currRow++;
-            space.textContent = event.key.toUpperCase();
-        }
-        
-    }else if (event.keyCode == 8){ //backspace
-        if(space != null){
-            space.style.border = "none"
-            space.style.animation = ""
-        } 
-        if(currRow>0) currRow--;
-        space.textContent = ""
-        space = document.getElementById(`column_${currColumn}:row_${currRow}`);
-        space.style.border = "2px solid black"
-        space.style.animation = "spaceAnim 0.5s forwards 0.1s infinite"
-
-    } else if(event.keyCode == 13){//enter
-        for(let x = 0; x<5 ; x++){//Chequeando que es lo que esta bien
-            let currspace = document.getElementById(`column_${currColumn}:row_${x}`)
-            if (currspace.textContent == word[x]){
-                currspace.style.backgroundColor = "green";
-                currspace.style.color = "white";
-                corrects++
-            }
-            else if(word.includes(currspace.textContent)){
-                currspace.style.backgroundColor = "yellow";
-                currspace.style.color = "black";
-            }else{
-                currspace.style.backgroundColor = "darkslategray";
-                currspace.style.color = "white";
-            }
-        }
-        if(corrects == 5){
-            EndGame()
-        }else{
+function NextColumn(){
             corrects = 0//reiniciamos el chequeo de win
             currColumn++
             if(currColumn>4){//se te acabaron las columns
-                EndGame()
+                setTimeout(() => {
+                    EndGame()
+                }, 1000);
             }
             currRow = 0;//posicionamos
             if(space != null){
@@ -85,6 +43,76 @@ window.addEventListener("keyup", ()=>{
             space = document.getElementById(`column_${currColumn}:row_${currRow}`);
             space.style.border = "2px solid black"
             space.style.animation = "spaceAnim 0.5s forwards 0.1s infinite"
+}
+
+function WinCheck(){
+    for(let x = 0; x<5 ; x++){//Chequeando que es lo que esta bien
+        let currspace = document.getElementById(`column_${currColumn}:row_${x}`)
+        if (currspace.textContent == word[x]){
+            setTimeout(()=>{
+                currspace.style.backgroundColor = "green";
+                currspace.style.color = "white";
+                currspace.style.animation = "spaceAnimBack 0.5s forwards 0s 1"
+            },200 + 200*x)
+            corrects++
+        }
+        else if(word.includes(currspace.textContent)){
+            setTimeout(()=>{
+                currspace.style.backgroundColor = "yellow";
+                currspace.style.color = "black";
+                currspace.style.animation = "spaceAnimBack 0.5s forwards 0s 1"
+            },200 + 200*x)
+        }else{
+            setTimeout(()=>{
+                currspace.style.backgroundColor = "darkslategray";
+                currspace.style.color = "white";
+                currspace.style.animation = "spaceAnimBack 0.5s forwards 0s 1"
+            },200 + 200*x)
+        }
+    }
+}
+function AddLetter(){
+    if(currRow<5){
+        if(space != null){
+            space.style.border = "none"
+            space.style.animation = ""
+        } 
+        space = document.getElementById(`column_${currColumn}:row_${currRow}`);
+        space.style.border = "2px solid black"
+        space.style.animation = "spaceAnim 0.5s forwards 0.1s infinite"
+        currRow++;
+        space.textContent = event.key.toUpperCase();
+    }
+}
+
+function DeleteLetter(){
+    if(space != null){
+        space.style.border = "none"
+        space.style.animation = ""
+    } 
+    if(currRow>0) currRow--;
+    space.textContent = ""
+    space = document.getElementById(`column_${currColumn}:row_${currRow}`);
+    space.style.border = "2px solid black"
+    space.style.animation = "spaceAnim 0.5s forwards 0.1s infinite"
+}
+window.addEventListener("keyup", ()=>{
+    if(event.keyCode != 8 && event.keyCode != 13){
+        //Si no es ni enter ni backspace
+        AddLetter()
+        
+    }else if (event.keyCode == 8){ //backspace
+        DeleteLetter()
+
+    } else if(event.keyCode == 13){//enter
+        WinCheck()
+        if(corrects == 5){
+            setTimeout(() => {
+                EndGame()
+            }, 1000);
+            
+        }else{
+            NextColumn()
         }
        
     }
